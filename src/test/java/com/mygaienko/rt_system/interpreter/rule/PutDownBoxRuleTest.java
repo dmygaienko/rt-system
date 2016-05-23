3a73f805c4b0ccbbf9d634da8a1493e7284c1e03
dmygaenko <dmygaenko@gmail.com>
2016-05-23 15:08:32 +0300

    moved tests

---
+++ src/test/java/com/mygaienko/rt_system/interpreter/rule/PutDownBoxRuleTest.java
@@ -0,0 +1,21 @@
+package com.mygaienko.rt_system.interpreter.rule;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dmygaenko on 23/05/2016.
+ */
+public class PutDownBoxRuleTest {
+
+    @Test
+    public void testIsApplied() throws Exception {
+
+    }
+
+    @Test
+    public void testProcess() throws Exception {
+
+    }
+}
